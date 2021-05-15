5be12b008c236301440eaf7387ccebf31349a81c
rahmanarif47 <rahmanarif.9764@gmail.com>
2021-05-15 18:06:01 +0700

    first commit

--- SwiftUIForm/ContentView.swift
+++ SwiftUIForm/ContentView.swift
@@ -9,72 +9,60 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+   
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsLetter = false
+    @State private var numberOfLikes = 4
+    
+    var body: some View{
+        NavigationView{
+            Form {
+                Section(header: Text("Personal Information")) {
+                    TextField("First Name", text : $firstName)
+                    TextField("First Name", text : $lastName)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Actions")){
+                    Toggle("Send Newsletter", isOn: $shouldSendNewsLetter).toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Term of Service", destination: URL(string: "https://agnikamika.wordpress.com")!)
+                }
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            .navigationTitle("Account")
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyboard()
+                    } label : {
+                        Image(systemName: "keyboard")
+                    }
+                    
+                    Button("Save", action: saveUser)
+                }
             }
         }
+        .accentColor(.red)
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    func saveUser() {
+        print("User Saved")
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
